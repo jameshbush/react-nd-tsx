f0c9dd2df0917cbf550d67e3402375581982abf5
James Bush <jbush@developertown.com>
2017-09-18 16:45:07 -0400

    Update index

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const render = (AppComponent: any) => {
     <AppContainer>
       <AppComponent />
     </AppContainer>,
-    document.getElementById("app"),
+    document.getElementById("root"),
   );
 };
 
